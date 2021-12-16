1770dd429b7a4c24392c3c9d9fe059e95a6e4293
AlbaSly <arias.brando.raxel@gmail.com>
2021-12-15 23:38:24 -0600

    Remove imports

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Character } from '../interfaces/dbz.interface';
-import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
